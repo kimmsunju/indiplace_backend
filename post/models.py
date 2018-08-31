4b9f6076142c73a68f1b1b499d7b3078f9b587d2
sunju <skrlatjswn@naver.com>
2018-08-31 17:16:00 +0900

    gitignored

--- post/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Member(models.Model):
-    loginid = models.CharField(max_length=10)
-    loginpw = models.CharField(max_length=15)
--- post/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.MemberList.as_view()),
-    path('<int:pk>/', views.MemberDetail.as_view())
-]
